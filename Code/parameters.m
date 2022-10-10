859567488101697d7e8ffae91f8cfa64fd221d7f
Vishnu Sharma HM <96663843+VishnuSharmaHM@users.noreply.github.com>
2022-10-10 19:37:09 -0400

    Add files via upload

---
+++ Code/parameters.m
@@ -0,0 +1,25 @@
+%% Parameters
+
+NR_TASK = 49;            % Number of Patients
+NR_LOC = 49;                % Number of locations
+NR_LOC_Mul=1;                           
+
+EPSILON_FIRST = 1;              % Epsilon: priavcy budget
+EPSILON_LAST = 1;              % Epsilon: priavcy budget
+EPSILON_INCR= 1;              % Epsilon: priavcy budget
+
+DELTA_FIRST = 1;
+DELTA_LAST = 3; 
+DELTA_INCR = 1;
+
+ITER_MAX = 10; 
+
+DELTA = 1;
+
+MIN_DISTANCE = 0.37; 
+% MIN_DISTANCE = 0.96; 
+
+
+GEOI_RANGE = MIN_DISTANCE*5;
+
+PROB_LB = 0.0; 
